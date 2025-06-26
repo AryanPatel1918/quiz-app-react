a9c32064172bec1b0b17d97e52184f0257ff003e
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-06-26 14:31:19 -0400

    Add questionsData file

---
+++ src/questionsData.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        question: "What is the capital of France?",
+        options: ['Madrid', 'Paris', 'Berlin', 'Rome'],
+        answer: "Paris"
+    },
+    {
+        question: "Which language runs in the browser?",
+        options: ['Python', 'C', 'Java', 'JavaScript'],
+        answer: "JavaScript"
+    },
+    {
+        question: "What does CSS stand for?",
+        options: [
+            'Computer Style Sheets',
+            'Colorful Style Sheets',
+            'Cascading Style Sheets',
+            'Creative Style Sheets'
+        ],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "Who discovered gravity?",
+        options: [
+            'Albert Einstein',
+            'Isaac Newton',
+            'Galileo Galilei',
+            'Johannes Kepler'
+            ],
+        answer: "Isaac Newton"
+    }
+]
